b4bcb5da5e4772136d42dc873d93ae5ce2f88706
david <dck003@ucsd.edu>
2024-06-05 21:39:32 -0700

    Finalized scene 4

--- scene-4.js
+++ scene-4.js
@@ -11,4 +11,34 @@ function cleanupScene4()
 
 }
 
-boxBackground.addEventListener('click', ()=>{boxBackground.style.backgroundColor="var(--Flame)"})
+function changeGradient(startPercent1, endPercent1, startPercent2, endPercent2, duration, callback) {
+    var start = null;
+    function step(timestamp) {
+        if (!start) start = timestamp;
+        var progress = timestamp - start;
+        var percentage1 = Math.min(startPercent1 + (progress / duration) * (endPercent1 - startPercent1), endPercent1);
+        var percentage2 = Math.min(startPercent2 + (progress / duration) * (endPercent2 - startPercent2), endPercent2);
+        boxBackground.style.background = `radial-gradient(circle, var(--Flame) ${percentage1}%, var(--AirSuperiorityBlue) ${percentage2}%)`;
+        if (progress < duration) {
+            window.requestAnimationFrame(step);
+        } else if (callback) {
+            callback();
+        }
+    }
+    window.requestAnimationFrame(step);
+}
+boxBackground.addEventListener("click", function() {
+    changeGradient(0, 0, 0, 100, 500, function() {
+        changeGradient(0, 100, 100, 100, 500);
+    });
+});
+
+boxBackground.addEventListener("click", ()=>{
+        document.getElementById('poem-scene-4').classList.add('shake-animation');
+        setTimeout(() => {
+            document.getElementById('poem-scene-4').classList.remove('shake-animation');
+            setTimeout(() => {
+                boxBackground.style.background = "var(--AirSuperiorityBlue)"
+            }, 250);
+    
+}, 2500); })
